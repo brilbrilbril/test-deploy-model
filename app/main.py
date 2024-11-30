3ed87b191ecd295c43279395bf3a234d9d3a455b
brilbrilbril <maghatan@gmail.com>
2024-11-30 19:30:07 +0700

    add another function to check route

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "Fastapi in container, check before deploy the model"}
+
+@app.get("/task")
+def perform_task():
+    return {"Task": "This is another task executed via a different route"}
